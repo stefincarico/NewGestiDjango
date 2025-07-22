2aa4790a38cf3ed020d744f0d2b46c76a363cd13
Stefano Incarico <stefano.incarico@outlook.it>
2025-07-22 17:19:23 +0200

    Modulo anagrafe implementato model e admin - normalizzazione dati anagrafici

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import AliquotaIVA, ModalitaPagamento
+from .models import (
+    AliquotaIVA, ModalitaPagamento,
+    Cliente, Fornitore, Dipendente
+)
 
 @admin.register(AliquotaIVA)
 class AliquotaIVAAdmin(admin.ModelAdmin):
@@ -41,6 +44,64 @@ class ModalitaPagamentoAdmin(admin.ModelAdmin):
     exclude = ('created_by', 'updated_by')
     
     # Mantiene la logica di salvataggio automatico
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome_cognome_ragione_sociale', 'partita_iva', 'codice_fiscale', 'citta', 'attivo')
+    search_fields = ('nome_cognome_ragione_sociale', 'partita_iva', 'codice_fiscale')
+    list_filter = ('attivo', 'citta')
+    exclude = ('created_by', 'updated_by')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Fornitore)
+class FornitoreAdmin(admin.ModelAdmin):
+    list_display = ('nome_cognome_ragione_sociale', 'partita_iva', 'codice_fiscale', 'citta', 'attivo')
+    search_fields = ('nome_cognome_ragione_sociale', 'partita_iva', 'codice_fiscale')
+    list_filter = ('attivo', 'citta')
+    exclude = ('created_by', 'updated_by')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Dipendente)
+class DipendenteAdmin(admin.ModelAdmin):
+    list_display = ('nome_cognome_ragione_sociale', 'mansione', 'data_assunzione', 'attivo')
+    search_fields = ('nome_cognome_ragione_sociale', 'mansione')
+    list_filter = ('attivo', 'mansione')
+    exclude = ('created_by', 'updated_by')
+    
+    # Con fieldsets possiamo raggruppare i campi nel form
+    fieldsets = (
+        ('Dati Anagrafici', {
+            'fields': (
+                'nome_cognome_ragione_sociale', 
+                ('codice_fiscale', 'partita_iva'),
+                'attivo'
+            )
+        }),
+        ('Dati di Contatto', {
+            'fields': ('indirizzo', 'cap', 'citta', 'provincia', 'email', 'telefono')
+        }),
+        ('Dati Lavorativi', {
+            'fields': ('mansione', 'data_assunzione', 'data_fine_rapporto', 'costo_orario', 'note_generali')
+        }),
+    )
+
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.created_by = request.user
--- core/models.py
+++ core/models.py
@@ -40,3 +40,70 @@ class ModalitaPagamento(TenantBaseModel):
     class Meta:
         verbose_name = "Modalità di Pagamento"
         verbose_name_plural = "Modalità di Pagamento"
+
+# === ANAGRAFICHE ===
+# Questo è il modello genitore che contiene tutti i campi comuni.
+
+class Anagrafica(TenantBaseModel):
+    nome_cognome_ragione_sociale = models.CharField(max_length=255)
+    codice_fiscale = models.CharField(max_length=16, blank=True, help_text="Valido sia per persone fisiche che giuridiche")
+    partita_iva = models.CharField(max_length=11, blank=True)
+
+    # Indirizzo
+    indirizzo = models.CharField(max_length=255, blank=True)
+    cap = models.CharField(max_length=5, blank=True)
+    citta = models.CharField(max_length=100, blank=True)
+    provincia = models.CharField(max_length=2, blank=True)
+
+    # Contatti
+    email = models.EmailField(blank=True)
+    telefono = models.CharField(max_length=50, blank=True)
+
+    def save(self, *args, **kwargs):
+        # Normalizzazione dei campi prima di salvare
+        self.nome_cognome_ragione_sociale = self.nome_cognome_ragione_sociale.upper()
+        if self.codice_fiscale:
+            self.codice_fiscale = self.codice_fiscale.upper()
+        if self.indirizzo:
+            self.indirizzo = self.indirizzo.title()
+        if self.citta:
+            self.citta = self.citta.title()
+        if self.provincia:
+            self.provincia = self.provincia.upper()
+        
+        super().save(*args, **kwargs) # Chiama il metodo save() originale per salvare effettivamente   
+        
+    class Meta:
+        verbose_name = "Anagrafica"
+        verbose_name_plural = "Tutte le Anagrafiche"
+
+    def __str__(self):
+        return self.nome_cognome_ragione_sociale
+
+
+# --- Modelli Figli ---
+# Ognuno eredita tutti i campi da Anagrafica e aggiunge i suoi specifici.
+
+class Cliente(Anagrafica):
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clienti"
+
+
+class Fornitore(Anagrafica):
+    class Meta:
+        verbose_name = "Fornitore"
+        verbose_name_plural = "Fornitori"
+
+
+class Dipendente(Anagrafica):
+    # Qui mettiamo i campi che erano in 'DipendenteDettaglio'
+    mansione = models.CharField(max_length=255)
+    data_assunzione = models.DateField()
+    data_fine_rapporto = models.DateField(null=True, blank=True)
+    costo_orario = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    note_generali = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Dipendente"
+        verbose_name_plural = "Dipendenti"
